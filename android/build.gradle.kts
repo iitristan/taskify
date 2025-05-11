d67511da2d12b51125ad3a94ff9f5e74e2ce0029
tristan <88244585+iitristan@users.noreply.github.com>
2025-05-11 22:32:21 +0800

    added firebase setup

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -5,6 +5,10 @@ allprojects {
     }
 }
 
+dependencies {
+    classpath("com.google.gms:google-services:4.4.0")
+}
+
 val newBuildDir: Directory = rootProject.layout.buildDirectory.dir("../../build").get()
 rootProject.layout.buildDirectory.value(newBuildDir)
 
